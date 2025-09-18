0279e2df83f473c9d86a9e35175fb4c32f7159a2
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2025-09-17 23:33:15 -0600

    :sparkles: Add short declaration topic

---
+++ 01-basic/02-short-declaration/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 42 + 1 // Declaracion corta
+	y := "Cesar Villalobos Olmos"
+	fmt.Println(x)
+	fmt.Println(y)
+
+	x = 50
+	fmt.Println(x)
+}
