88bbf9a2b24751515c8794fb2fa05d05cea26f24
Cesar Villalobos Olmos <cesarvillalobosolmos.01@gmail.com>
2025-09-21 23:46:12 -0600

    :sparkles: Add exercise 03

---
+++ 02-types/05-excersices/03.go
@@ -0,0 +1,25 @@
+/* 
+Usando el código del ejercicio anterior,
+1. En scope a nivel de paquete, asigna los siguientes valores a las tres variables
+	a. a x asignale 42
+	b. a y asignale “James Bond”
+	c. a z asignale true
+2. en main
+	a. Usa fmt.Sprintf para imprimir todos los VALORES en un solo string. ASIGNA el
+	valor retornado de TIPO string usando el operador de declaración corta a la
+	VARIABLE con el IDENTIFICADOR “s”
+	b. Imprime el valor almacenado por la variable “s”
+*/
+
+package main
+
+import "fmt"
+
+var x int = 42
+var y string = "James Bond"
+var z bool = true
+
+func main() {
+	s := fmt.Sprintf("%v\t%v\t%v", x, y, z)
+	fmt.Println(s)
+}
